1150c8a27f3089e01ee10ec4e26c19498c73fbf4
José Marinho <jose.silva@bandtec.com.br>
2025-06-08 23:14:42 -0300

    feat: logging message as json stringify if message is a object

--- src/log/log-service.service.ts
+++ src/log/log-service.service.ts
@@ -14,21 +14,60 @@ export class LogService extends Logger {
 
   public log(message: any, ...optionalParams: any[]): void {
     const traceId = this.cls.getId();
+    // If message is not an object, log it directly
+    // with the traceId
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message);
+    }
+    // Use super.log to ensure the message is logged correctly
+    // with the traceId and any optional parameters
+    // This ensures that the message is always an object
+    // and includes the traceId
+    // This is important for structured logging
     super.log({ message: message, traceId: traceId }, ...optionalParams);
   }
 
   public debug(message: any, ...optionalParams: any[]): void {
     const traceId = this.cls.getId();
+    // If message is not an object, log it directly
+    // with the traceId
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message);
+    }
+    // Use super.debug to ensure the message is logged correctly
+    // with the traceId
+    // and any optional parameters
+    // This ensures that the message is always an object
+    // and includes the traceId
     super.debug({ message: message, traceId: traceId }, ...optionalParams);
   }
 
   public warn(message: any, ...optionalParams: any[]): void {
     const traceId = this.cls.getId();
+    // If message is not an object, log it directly
+    // with the traceId
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message);
+    }
+    // Use super.warn to ensure the message is logged correctly
+    // with the traceId and any optional parameters
+    // This ensures that the message is always an object
+    // and includes the traceId
     super.warn({ message: message, traceId: traceId }, ...optionalParams);
   }
 
   public error(message: any, ...optionalParams: any[]): void {
     const traceId = this.cls.getId();
+    // If message is not an object, log it directly
+    // with the traceId
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message);
+    }
+    // Use super.error to ensure the message is logged correctly
+    // with the traceId and any optional parameters
+    // This ensures that the message is always an object
+    // and includes the traceId
+    // This is important for structured logging
     super.error({ message: message, traceId: traceId }, ...optionalParams);
   }
 }
